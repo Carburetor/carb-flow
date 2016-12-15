0067cf5d282f4a65d82096e51837bb120ce71882
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2016-12-15 15:35:57 +0100

    Added tests for action list

---
+++ lib/carb/flow/transaction/action_list.rb
@@ -0,0 +1,45 @@
+require "carb/flow"
+require "carb/flow/transaction"
+require "carb/flow/transaction/action"
+
+module Carb::Flow
+  class Transaction::ActionList
+    private
+
+    attr_reader :list
+
+    public
+
+    def initialize(action_list = nil)
+      @list = extract_list_or_empty(action_list)
+    end
+
+    def <<(action)
+      unless action.is_a?(::Carb::Flow::Transaction::Action)
+        raise TypeError, "action must be an Action"
+      end
+
+      list << action
+    end
+
+    def to_a
+      list
+    end
+
+    # @yieldparam action [Action]
+    # @yieldparam is_last [Boolean] if it's the last action of the list
+    def each
+      list.each_with_index do |action, index|
+        yield(action, ((index + 1) == list.size))
+      end
+    end
+
+    private
+
+    def extract_list_or_empty(list)
+      return [] if list.nil?
+
+      list.to_a
+    end
+  end
+end
---
+++ spec/unit/carb/flow/transaction/action_list_spec.rb
@@ -0,0 +1,56 @@
+require "spec_helper"
+require "carb/flow/transaction/action"
+require "carb/flow/transaction/action_list"
+
+describe Carb::Flow::Transaction::ActionList do
+  before do
+    @action      = Carb::Flow::Transaction::Action.new(:foo, :bar, [123])
+    @action_list = Carb::Flow::Transaction::ActionList.new
+  end
+
+  it "can be initialized with another list" do
+    @action_list << @action
+
+    other = Carb::Flow::Transaction::ActionList.new(@action_list)
+
+    expect(@action_list.to_a).to eq [@action]
+  end
+
+  it "has support for adding actions" do
+    @action_list << @action
+  end
+
+  it "can't add non-action objects" do
+    expect{@action_list << 123}.to raise_error TypeError
+  end
+
+  it "can be converted to array" do
+    @action_list << @action
+
+    expect(@action_list.to_a).to eq [@action]
+  end
+
+  it "loops over each action" do
+    looped_times = 0
+    @action_list << @action
+
+    @action_list.each { |_, _| looped_times += 1 }
+
+    expect(looped_times).to eq 1
+  end
+
+  it "yields is_last true only on last action" do
+    action2      = Carb::Flow::Transaction::Action.new(:baz, :blah, [456])
+    looped_times = 0
+    @action_list << action2
+    @action_list << @action
+
+    @action_list.each do |action, is_last|
+      looped_times += 1
+      expect(is_last).to be true   if looped_times == 2
+      expect(action).to eq @action if is_last
+    end
+
+    expect(looped_times).to eq 2
+  end
+end
